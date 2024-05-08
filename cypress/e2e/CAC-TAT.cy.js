2503934d6664894cbaad3ee3cfe830952e99bfec
Thiagoir <thiagoir2@hotmail.com>
2024-05-08 15:30:32 +0100

    update test failed

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -80,7 +80,7 @@ describe("Central de atentimento ao Cliente TAT", () => {
   it("marca o tipo de atendimento 'Feedback'", () => {
     cy.get('input[type="radio"][value="feedback"]')
       .check()
-      .should("have.value", "feedback");
+      .should("have.value", "feedbac");
   });
   it("marca cada tipo de atendimento", () => {
     cy.get('input[type="radio"]')
